0e4379eeb7ff95d764bb301a1ea94a52c1f6631d
Osama0habib <86162362+Osama0habib@users.noreply.github.com>
2022-09-06 22:46:11 +0200

    sqflite

--- lib/DatabaseUtil.dart
+++ lib/DatabaseUtil.dart
@@ -1,19 +1,54 @@
 import 'package:sqflite/sqflite.dart';
 
 class DatabaseUtil {
- static late Database db;
+  static late Database db;
+  static List<Map> tasks = [];
 
   static Future<void> openDB() async {
-    db = await openDatabase(
+     await openDatabase(
       'todo.db',
       version: 1,
       onCreate: (dbs, version) async {
-        await db.execute(
-            'CREATE TABLE tasks (id INTEGER PRIMARY KEY, title TEXT, body TEXT, time TEXT ,status INTEGER)');
+
+        print("database Created");
+        await dbs.execute(
+            'CREATE TABLE task (id INTEGER PRIMARY KEY, title TEXT, body TEXT, time TEXT ,status INTEGER)');
       },
-      onOpen: (db){
-        
+      onOpen: (dbs) async {
+        db = dbs;
+        print("database Opened");
+        // await getFromDB();
+       // await db.insert("task", {"title" : "flutter project", "body" : "learn about sqflite","time" : DateTime.now().toString() , "status" : 1});
       },
-    );
+    ).catchError((error) {});
   }
-}
+
+  static Future<void> insertToDatabase(title ,body , time )async{
+   await db.insert("task", {"title" : title , "body" : body , "time" : time ,"status" : 1}).then((value) {
+     getFromDataBase();
+   });
+  }
+
+
+  static Future<void> getFromDataBase()async{
+    await db.query("task",).then((task) {
+      print(task);
+      tasks = task;
+    });
+  }
+
+  void updateToDatabase(id,title ,body , time ,status )async{
+    await db.update("task", {"title" : title , "body" : body , "time" : time ,"status" : status},where: "id = $id");
+  }
+
+  void deleteFromDatabase(id)async{
+    await db.delete("task", where: id);
+  }
+  //
+  // static getFromDB()async {
+  //   await db.query('task',).then((value) {
+  //     tasks = value;
+  //     print(tasks);
+  //  });
+  }
+// }
--- lib/screens/tasks.dart
+++ lib/screens/tasks.dart
@@ -1,30 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:todo_app_couree/DatabaseUtil.dart';
 import 'package:todo_app_couree/screens/widgets/list_item.dart';
 
 class Tasks extends StatelessWidget {
    Tasks({Key? key}) : super(key: key);
 
 
-  List<String> strings = [
-    "ahmed",
-    "حسام",
-    "3treees",
-    "zed",
-    "3beeed",
-    "ahmed",
-    "mohamoed",
-    "3treees",
-    "zed",
-    "3beeed",
-  ];
+  // List<String> strings = [
+  //   "ahmed",
+  //   "حسام",
+  //   "3treees",
+  //   "zed",
+  //   "3beeed",
+  //   "ahmed",
+  //   "mohamoed",
+  //   "3treees",
+  //   "zed",
+  //   "3beeed",
+  // ];
 
   @override
   Widget build(BuildContext context) {
+   List<Map> tasks = DatabaseUtil.tasks;
     return ListView.builder(
-      itemCount : strings.length ,
+      itemCount : tasks.length ,
       itemBuilder: (BuildContext context, int index) {
-      return  ListItem(strings: strings, index: index,
-      );
+      return  ListItem( task: tasks[index],);
       },);
   }
 }
